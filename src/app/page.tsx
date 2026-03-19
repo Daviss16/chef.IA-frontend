4e4b9c62d45d45f88c36ecd0430594888c881c82
Daviss16 <davisilvadesouza1616@gmail.com>
2026-03-19 17:55:48 -0300

    chore: scaffolding inicial do frontend

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Hello() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontFamily: 'sans-serif' }}>
+    <h1>Hello World, Welcome to Chef.IA your new AI waiter!!!!!!!!</h1>
+    </div>
+  );
+  
+}
